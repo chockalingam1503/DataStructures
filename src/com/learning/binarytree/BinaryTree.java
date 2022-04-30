877ceebb378becab662b0a32e3d7c7c99c737b02
chocku1989 <chocku1989@gmail.com>
2022-04-30 19:09:10 +0530

    CHanges

--- src/com/learning/binarytree/BinaryTree.java
+++ src/com/learning/binarytree/BinaryTree.java
@@ -49,17 +49,19 @@ public class BinaryTree {
 	  
 
 	  public BinaryTree createBinaryTree() {
-		    BinaryTree bt = new BinaryTree();
-
-		    bt.add(6);
-		    bt.add(4);
-		    bt.add(8);
-		    bt.add(3);
-		    bt.add(5);
-		    bt.add(7);
-		    bt.add(9);
-
-		    return bt;
+		    try {
+				BinaryTree bt = new BinaryTree();
+				bt.add(6);
+				bt.add(4);
+				bt.add(8);
+				bt.add(3);
+				bt.add(5);
+				bt.add(7);
+				bt.add(9);
+				return bt;
+			} finally {
+				System.out.println("I am inally");
+			}
 		}
 	  
 //	  @Test
